38c35429b117366d801e56ead5fa6eb11892bb36
PaLucena <pablolucena01@gmail.com>
2024-06-26 20:10:30 +0200

    feat: Page change

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.getElementById("Home").innerHTML = "●";
 
 function	pressBtn(btn) {
 	if (btn.innerHTML !== "●") {
@@ -7,5 +6,13 @@ function	pressBtn(btn) {
 				element.innerHTML = element.id;
 		})
 		btn.innerHTML = "●";
+		showContent(btn);
 	}
+}
+
+function	showContent(btn) {
+	document.querySelectorAll("[id^='content-']").forEach(element => {
+		element.style.display = "none";
+	});
+	document.querySelector("#content-" + btn.id).style.display = "block";
 }
